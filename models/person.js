2d7fec16927a41a93dcee6f2a55df29d8db5b0f3
Duras <walendes@gmail.com>
2020-07-16 07:39:14 +0200

    phonebook done

--- models/person.js
+++ models/person.js
@@ -20,7 +20,7 @@ mongoose
 const personSchema = new mongoose.Schema({
   name: { type: String, minlength: 3, required: true, unique: true },
   number: { type: String, minlength: 8, required: true, unique: true },
-  date: { type: Date() },
+  date: { type: Date },
   id: { type: Number }
 })
 
